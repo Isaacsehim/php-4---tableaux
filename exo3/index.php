c98195a25391b9f179ef1a46a20763bcc4047e5f
Isaac Sehim <isaacsehim@gmail.com>
2025-02-23 23:24:09 +0100

    final commit

---
+++ exo3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Exercice 3</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<h1>EXERCICE n°3 PHP</h1>
+<div class="container">
+<?php
+require_once 'products.php';
+?>
+</div>
+</body>
+</html>
--- exo3/products.php
+++ exo3/products.php
@@ -5,105 +5,105 @@
 			'nom' => 'Alienware M18 R2',
 			'prix' => '2000',
 			'description' => 'Ordinateur portable gaming haute performance',
-			'image' => 'img/alienware_m18_r2.png'
+			'image' => './assets/img/alienware_m18_r2.png'
 		],
 		[
 			'categorie' => 'Souris Gaming',
 			'nom' => 'Logitech G502',
 			'prix' => '80',
 			'description' => 'Souris gaming ergonomique',
-			'image' => 'img/logitech_g502.png'
+			'image' => './assets/img/logitech_g502.png'
 		],
 		[
 			'categorie' => 'Clavier Gaming',
 			'nom' => 'Corsair K65 RGB MINI 60%',
 			'prix' => '150',
 			'description' => 'Clavier mécanique RGB',
-			'image' => 'img/corsair_k65_rgb.png'
+			'image' => './assets/img/corsair_k65_rgb.png'
 		],
 		[
 			'categorie' => 'Moniteur Gaming',
 			'nom' => 'Asus ROG Swift OLED PG27UCDM',
 			'prix' => '1000',
 			'description' => 'Moniteur gaming 4K',
-			'image' => 'img/asus_rog_swift.png'
+			'image' => './assets/img/asus_rog_swift.png'
 		],
 		[
 			'categorie' => 'Chaise Gaming',
 			'nom' => 'Secretlab Titan evo',
 			'prix' => '400',
 			'description' => 'Chaise gaming confortable',
-			'image' => 'img/secretlab_titan_evo.png'
+			'image' => './assets/img/secretlab_titan_evo.png'
 		],
 		[
 			'categorie' => 'Casque Gaming',
 			'nom' => 'SteelSeries Arctis 8',
 			'prix' => '150',
 			'description' => 'Casque gaming surround',
-			'image' => 'img/steelseries_arctis_8.png'
+			'image' => './assets/img/steelseries_arctis_8.png'
 		],
 		[
 			'categorie' => 'Carte Graphique Gaming',
 			'nom' => 'NVIDIA GeForce RTX 5090',
 			'prix' => '2400',
 			'description' => 'Carte graphique haut de gamme',
-			'image' => 'img/nvidia_geforce_rtx_5090.png'
+			'image' => './assets/img/nvidia_geforce_rtx_5090.png'
 		],
 		[
 			'categorie' => 'Bureau Gaming',
 			'nom' => 'Arozzi Arena',
 			'prix' => '300',
 			'description' => 'Bureau gaming spacieux',
-			'image' => 'img/arozzi_arena.png'
+			'image' => './assets/img/arozzi_arena.png'
 		],
 		[
 			'categorie' => 'Manette',
 			'nom' => 'Xbox Elite 2 Controller',
 			'prix' => '180',
 			'description' => 'Manette gaming sans fil',
-			'image' => 'img/xbox_elite2_controller.png'
+			'image' => './assets/img/xbox_elite2_controller.png'
 		],
 		[
 			'categorie' => 'Manette',
 			'nom' => 'DualShock 5',
 			'prix' => '60',
 			'description' => 'Manette gaming sans fil',
-			'image' => 'img/dualshock5.png'
+			'image' => './assets/img/dualshock5.png'
 		],
 		[
 			'categorie' => 'Routeur Gaming',
 			'nom' => 'Netgear Nighthawk Pro',
 			'prix' => '250',
 			'description' => 'Routeur gaming haute vitesse',
-			'image' => 'img/netgear_nighthawk_pro.png'
+			'image' => './assets/img/netgear_nighthawk_pro.png'
 		],
 		[
 			'categorie' => 'Tapis de Souris Gaming',
 			'nom' => 'SteelSeries QcK XXL',
 			'prix' => '40',
 			'description' => 'Grand tapis de souris gaming',
-			'image' => 'img/steelseries_qck_xxl.png'
+			'image' => './assets/img/steelseries_qck_xxl.png'
 		],
 		[
 			'categorie' => 'Enceintes Gaming',
 			'nom' => 'Logitech Z906',
 			'prix' => '400',
 			'description' => 'Enceintes gaming haute qualité',
-			'image' => 'img/logitech_z906.png'
+			'image' => './assets/img/logitech_z906.png'
 		],
 		[
 			'categorie' => 'Microphone Gaming',
 			'nom' => 'Blue Yeti',
 			'prix' => '130',
 			'description' => 'Microphone gaming professionnel',
-			'image' => 'img/blue_yeti.png'
+			'image' => './assets/img/blue_yeti.png'
 		],
 		[
 			'categorie' => 'Webcam',
 			'nom' => 'Logitech StreamCam',
 			'prix' => '170',
 			'description' => 'Webcam gaming HD',
-			'image' => 'img/logitech_streamcam.png'
+			'image' => './assets/img/logitech_streamcam.png'
 		]
 	];
 
@@ -112,7 +112,7 @@
 		'nom' => 'Logitech G502 X PLUS',
 		'prix' => '160',
 		'description' => "Souris ultra rapide dpi de l'extrême",
-		'image' => 'img/logitech_g502_x.png'
+		'image' => './assets/img/logitech_g502_x.png'
 	]);
 
 	foreach ($products as $product) {
